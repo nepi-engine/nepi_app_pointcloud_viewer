ac9f51ab781fc911a37a72f1bc019dc8ecebdd5f
Jason Seawall <jseawall@numurus.com>
2025-04-21 07:49:54 -0700

    3p2p0 RC5C updates

--- scripts/pointcloud_viewer_app_node.py
+++ scripts/pointcloud_viewer_app_node.py
@@ -55,7 +55,7 @@ from nepi_sdk import nepi_img
 
 
 from nepi_api.node_if import NodeClassIF
-from nepi_api.sys_if_msg import MsgIF
+from nepi_api.messages_if import MsgIF
 from nepi_api.sys_if_save_data import SaveDataIF
 from nepi_api.sys_if_save_cfg import SaveCfgIF
 
@@ -523,7 +523,7 @@ class NepiPointcloudViewerApp(object):
 
 
     # Create Node Class ####################
-    self.node_if = NodeClassIF(self,
+    self.node_if = NodeClassIF(
                     configs_dict = self.CFGS_DICT,
                     params_dict = self.PARAMS_DICT,
                     pubs_dict = self.PUBS_DICT,
