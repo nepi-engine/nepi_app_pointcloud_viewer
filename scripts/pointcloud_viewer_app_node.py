f6001a6b20ac5d076733d00de9fa9fd7d237b202
Jason Seawall <jseawall@numurus.com>
2025-07-09 16:38:17 -0700

    **

--- scripts/pointcloud_viewer_app_node.py
+++ scripts/pointcloud_viewer_app_node.py
@@ -583,7 +583,6 @@ class NepiPointcloudViewerApp(object):
 
   def initCb(self,do_updates = False):
     if self.node_if is not None:
-
       pass
 
     if do_updates == True:
@@ -591,14 +590,21 @@ class NepiPointcloudViewerApp(object):
     self.publish_status()
 
   def resetCb(self,do_updates = True):
-    if do_updates:
+      self.msg_if.pub_warn("Reseting")
+      if self.node_if is not None:
         pass
-    self.initCb(do_updates = do_updates)
+      if do_updates == True:
+        pass
+      self.initCb(do_updates = do_updates)
+
 
   def factoryResetCb(self,do_updates = True):
-    if do_updates:
+      self.msg_if.pub_warn("Factory Reseting")
+      if self.node_if is not None:
         pass
-    self.initCb(do_updates = do_updates)
+      if do_updates == True:
+        pass
+      self.initCb(do_updates = do_updates)
 
 
   ###################
